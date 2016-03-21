f2d97577d8e809d41287c5d93581300db022be83
Stefan Eng <eng_steff@hotmail.com>
2016-03-21 21:17:09 +0100

    Extract all the titles and make title-test pass.

--- pagecounter/parser.py
+++ pagecounter/parser.py
@@ -2,14 +2,50 @@
 import re
 import sys
 
+from collections import OrderedDict
+
 from pagecounter import config
 
+# Global config object.
 conf = config.get()
+
+# Regular expressions.
+comment = r'^#'
 
 
 def parse(data):
 
-    return {}
+    data = data.split('\n')
+
+    non_comment_data = [line for line in data if not re.match(comment,
+                        line.strip())]
+
+    title_dict = title_pass(non_comment_data)
+
+    return title_dict
+
+
+def title_pass(data):
+
+    current_title = ""
+    current_data = []
+
+    title_data_dict = OrderedDict()
+
+    for line in data:
+        if re.match(r'^\S', line):
+            if current_title:
+                title_data_dict[current_title] = current_data
+                current_data = []
+            current_title = line
+        else:
+            current_data.append(line)
+
+    if current_data:
+        title_data_dict[current_title] = current_data
+
+    return title_data_dict
+
 
 def main(args):
 
