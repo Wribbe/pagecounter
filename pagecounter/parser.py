646b79eb8a30a0d568caf2f92e842867aa4847e0
Stefan Eng <eng_steff@hotmail.com>
2016-03-21 20:52:49 +0100

    Define failing parse title test.

--- pagecounter/parser.py
+++ pagecounter/parser.py
@@ -1,12 +1,27 @@
 
 import re
+import sys
 
 from pagecounter import config
 
 conf = config.get()
 
-def book_source():
-    return conf.book_source
+
+def parse(data):
+
+    return {}
+
+def main(args):
+
+    if args:
+        book_source = args[0]
+    else:
+        book_source = conf.book_source
+
+    with open(book_source, 'r') as file_pointer:
+        book_data = file_pointer.read()
+
+    parse(book_data)
 
 if __name__ == "__main__":
-    print(book_source())
+    main(sys.argv[1:])
--- pagecounter/tests/parser_test.py
+++ pagecounter/tests/parser_test.py
@@ -1,6 +1,38 @@
 
+test_book_data = """
+#This is a comment
+Title of book
+
+  Tags: Tag1, tag2, abcd
+  Total: 700
+  Exam: !today+7
+
+  Sections:
+
+    date: !today+1
+    0 [2-5]: 0-10
+
+    date: !today+2
+    1 [1,2]: 10-20
+
+    date: !today+3
+    1 [3-5]: 21-30
+
+Title of second book
+
+Title of third book
+"""
+
 from pagecounter import parser
 
-def test_book_source():
-    conf = parser.config.get()
-    assert(parser.book_source() == conf.book_source)
+def test_titles():
+
+    parser_result = parser.parse(test_book_data)
+
+    title_list = [
+            'Title of book',
+            'Title of second book',
+            'Title of third book',
+            ]
+
+    assert(list(parser_result.keys()) == title_list)
